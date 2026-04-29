645257091b9e406638b772de25598adefb49ba61
yang chen <yangchen@dhcp-10-31-166-138.dyn.MIT.EDU>
2026-04-29 12:50:04 -0400

    Redid the alcohol flight, seems normal now

--- example/example_PRD_Fuel.jl
+++ example/example_PRD_Fuel.jl
@@ -13,14 +13,19 @@ ac = load_default_model() #Use default model for payload-range diagram
 size_aircraft!(ac)
 
 #Run off-design on jet fuel
-idxFuel = 24
+idxFuel = 25
 rhoFuel = 817.0
 LHVaporFuel = 358694.0 #J/kg
 mPay_Lst, Ranges_Lst, PFEIs_Lst = PayloadRangeFuel(ac, idxFuel, rhoFuel, LHVaporFuel)
+println(Ranges_Lst,"\n")
+println(mPay_Lst,"\n")
+println(PFEIs_Lst,"\n")
+
 
 idxFuel = 32
 rhoFuel = 789.0
 LHVaporFuel = 918187.9 #J/kg
-println(Ranges_Lst)
-println(mPay_Lst)
-println(PFEIs_Lst)
+mPay_Lst, Ranges_Lst, PFEIs_Lst = PayloadRangeFuel(ac, idxFuel, rhoFuel, LHVaporFuel)
+println(Ranges_Lst,"\n")
+println(mPay_Lst,"\n")
+println(PFEIs_Lst,"\n")
