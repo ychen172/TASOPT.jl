ed7520098b11deed76c8f3e8e4c33f68c652cc25
yang chen <yangchen@dhcp-10-31-166-138.dyn.MIT.EDU>
2026-04-29 13:58:40 -0400

    Store the payload range data

--- example/example_PRD_Fuel.jl
+++ example/example_PRD_Fuel.jl
@@ -6,7 +6,13 @@ fleet of missions
 # 1. Import modules
 using TASOPT
 using Plots
-# import indices for calling parameters
+using CSV
+# Get directory
+savedir = "Movie/"
+if !isdir(savedir)
+    mkdir(savedir)
+end
+
 
 # Load default model
 ac = load_default_model() #Use default model for payload-range diagram
@@ -16,16 +22,23 @@ size_aircraft!(ac)
 idxFuel = 25
 rhoFuel = 817.0
 LHVaporFuel = 358694.0 #J/kg
-mPay_Lst, Ranges_Lst, PFEIs_Lst = PayloadRangeFuel(ac, idxFuel, rhoFuel, LHVaporFuel)
-println(Ranges_Lst,"\n")
-println(mPay_Lst,"\n")
-println(PFEIs_Lst,"\n")
+mPay_Jet, Ranges_Jet, PFEIs_Jet = PayloadRangeFuel(ac, idxFuel, rhoFuel, LHVaporFuel, Rpts = 40, Ppts = 41)
 
+Output = (; 
+    Symbol("mPay (Ton)") => mPay_Jet,
+    Symbol("Ranges (nmi)") => Ranges_Jet,
+    Symbol("PFEIs") => PFEIs_Jet
+)
+CSV.write("$(savedir)PayRanDataJetFuel.csv", Output; writeheader=true)
 
 idxFuel = 32
 rhoFuel = 789.0
 LHVaporFuel = 918187.9 #J/kg
-mPay_Lst, Ranges_Lst, PFEIs_Lst = PayloadRangeFuel(ac, idxFuel, rhoFuel, LHVaporFuel)
-println(Ranges_Lst,"\n")
-println(mPay_Lst,"\n")
-println(PFEIs_Lst,"\n")
+mPay_Eth, Ranges_Eth, PFEIs_Eth = PayloadRangeFuel(ac, idxFuel, rhoFuel, LHVaporFuel, Rpts = 40, Ppts = 41)
+
+Output = (; 
+    Symbol("mPay (Ton)") => mPay_Eth,
+    Symbol("Ranges (nmi)") => Ranges_Eth,
+    Symbol("PFEIs") => PFEIs_Eth
+)
+CSV.write("$(savedir)PayRanDataEthanol.csv", Output; writeheader=true)
