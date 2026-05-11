09819f93bf34e4e17806e5863846fb4f4de7daae
yang chen <yangchen@dhcp-10-29-213-176.dyn.MIT.EDU>
2026-05-10 22:53:16 -0400

    add additional parameters to print out

--- example/examine.jl
+++ example/examine.jl
@@ -49,10 +49,35 @@ TakeoffMass_Max = ac.parg[igWMTO]/9.81/1000. #(Ton)
 println("Takeoff Mass (Ton): Cur: $TakeoffMass_Cur, Max: $TakeoffMass_Max")
 
 # 3) Collect and save design parameters
+#Mission Parameters
 massPayload = ac.parm[imWpay, 1]/9.81/1000. #(Ton)
 numberPassengers = ac.parm[imWpay, 1]/ac.parm[imWperpax, 1] #Number
 Range = ac.parm[imRange, 1]/1852. #nmi
 PFEI = ac.parm[imPFEI, 1] #(J/J)
+#Input Optimized Parameter
+AR = ac.wing.layout.AR #Wing aspect ratio
+CL_cruise = ac.para[iaCL, ipcruise1, 1] #Cruise CL
+sweep = ac.wing.layout.sweep #Wing sweep angle (deg)
+alt_cruise = ac.para[iaalt, ipcruise1, 1]* 3.280839895 #Cruise altitude (ft)
+taper_wing_in = ac.wing.inboard.λ
+taper_wing_out = ac.wing.outboard.λ
+thick_to_chord_in = ac.wing.inboard.cross_section.thickness_to_chord
+thick_to_chord_out = ac.wing.outboard.cross_section.thickness_to_chord
+break_root_cl_ratio_cruise = ac.para[iarcls, ipcruise1, 1]
+tip_root_cl_ratio_cruise = ac.para[iarclt, ipcruise1, 1]
+Tt4_cruise = ac.pare[ieTt4, ipcruise1, 1] #(K)
+PR_hpc_cruise = ac.pare[iepihc, ipcruise1, 1] #HPC pressure ratio
+PR_fan_cruise = ac.pare[iepif, ipcruise1, 1] #Fan
+PR_lpc_cruise = ac.pare[iepilc, ipcruise1, 1] #LPC
+BPR_cruise = ac.pare[ieBPR, ipcruise1, 1] #bypass ratio at cruise
+#Parameters to Flight Efficiencies
+LD_cruise = 0.5 * (ac.para[iaCL, ipcruise1, 1]/ac.para[iaCD, ipcruise1, 1] + 
+                        ac.para[iaCL, ipcruise2, 1]/ac.para[iaCD, ipcruise2, 1]) #Averaged cruise lift-to-drag ratio
+LHV_cruise = 0.5 * (ac.pare[iehfuel, ipcruise1, 1] + ac.pare[iehfuel, ipcruise2, 1]) #Averaged cruise heating value (J/kg)
+TSFC_cruise = 0.5 * (ac.pare[ieTSFC, ipcruise1, 1] + ac.pare[ieTSFC, ipcruise2,1]) / gee #Averaged cruise thrust specfic heat consumption (kg/s/N)
+TSEC_cruise = TSFC_cruise*LHV_cruise #Averaged cruise thrust specific energy consumption (J/s/N)
+vel_cruise = 0.5 * (cos(ac.para[iagamV, ipcruise1,1]) * ac.pare[ieu0, ipcruise1,1] + 
+                    cos(ac.para[iagamV, ipcruise2,1]) * ac.pare[ieu0, ipcruise2, 1]) #Averaged cruise horizontal velocity (m/s)
 
 var_names = [
     "Payload Mass (Ton)",
@@ -66,7 +91,27 @@ var_names = [
     "Metal Temperature (K)",
     "Fan Diameter (m)",
     "Fuel Mass (Ton)",
-    "Takeoff Mass (Ton)"
+    "Takeoff Mass (Ton)",
+    "Wing Aspect Ratio",
+    "Cruise CL",
+    "Wing Sweep Angle (deg)",
+    "Cruise Altitude (ft)",
+    "Inboard Wing Taper Ratio",
+    "Outboard Wing Taper Ratio",
+    "Inboard Thickness-to-Chord Ratio",
+    "Outboard Thickness-to-Chord Ratio",
+    "Break/Root CL Ratio at Cruise",
+    "Tip/Root CL Ratio at Cruise",
+    "Tt4 at Cruise (K)",
+    "HPC Pressure Ratio at Cruise",
+    "Fan Pressure Ratio at Cruise",
+    "LPC Pressure Ratio at Cruise",
+    "Bypass Ratio at Cruise",
+    "Lift-to-Drag Ratio at Cruise",
+    "Heating Value at Cruise (J/kg)",
+    "Thrust Specific Fuel Consumption at Cruise (kg/s/N)",
+    "Thrust Specific Energy Consumption at Cruise (J/s/N)",
+    "Horizontal Velocity at Cruise (m/s)"
 ]
 
 val = [
@@ -81,7 +126,27 @@ val = [
     TMetal_Cur,
     DiaFan_Cur,
     FuelMass_Cur,
-    TakeoffMass_Cur
+    TakeoffMass_Cur,
+    AR,
+    CL_cruise,
+    sweep,
+    alt_cruise,
+    taper_wing_in,
+    taper_wing_out,
+    thick_to_chord_in,
+    thick_to_chord_out,
+    break_root_cl_ratio_cruise,
+    tip_root_cl_ratio_cruise,
+    Tt4_cruise,
+    PR_hpc_cruise,
+    PR_fan_cruise,
+    PR_lpc_cruise,
+    BPR_cruise,
+    LD_cruise,
+    LHV_cruise,
+    TSFC_cruise,
+    TSEC_cruise,
+    vel_cruise
 ]
 
 df = DataFrame(
