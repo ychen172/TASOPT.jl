f7656769430fe1592cb53654b3f0027757171483
yang chen <yangchen@yangs-MacBook-Pro.local>
2026-05-11 17:24:45 -0400

    Make the optimizatin function a call able function

--- example/optimize_rangefuel.jl
+++ example/optimize_rangefuel.jl
@@ -1,3 +1,6 @@
+module OptimizeRangeFuel
+export MissionReq, BoundsOpt, ConstraintsOpt, optimize_rangefuel_fun!
+
 using TASOPT, NLopt
 include(joinpath(@__DIR__, "objective_factory.jl"))
 using .ObjectiveFactory: OptHistory, make_obj, best_feasible
@@ -38,129 +41,141 @@ Base.@kwdef mutable struct ConstraintsOpt #Constrained values for optimization
 end
 
 """
-    optimize_aircraft!(ac; mission_req, constraints, lower, upper, init_dx,
-                       save_dir="ModelSaved", save_name="acOptimized_Cus2",
-                       tol_rel=1e-6, maxeval=1000, optimizer=:LN_NELDERMEAD)
+optimize_rangefuel_fun!(ac; mission_req=default, bounds_opt=default, constraints_opt=default
+                        tol_rel=default, maxeval=default, optimizer=default)
 
-Optimize aircraft design variables in-place on `ac`.
-
-Required:
-- mission_req: NamedTuple, e.g.
-    (range_m=3000*1852, ifuel=24, rhofuel=817.0, ehvap=358694.0, ehvapcombustor=358694.0)
-- constraints: [max_span, max_lenField, min_TOCGamma, max_Tt3, max_TMetal, max_DiaFan]
-- lower, upper, init_dx: vectors for 15 design variables
-
-Returns a NamedTuple with status, history, and save path.
+Optimize an aircraft for a design flight range and fuel type
+    Input aircraft model will be modified inplace to become the optimized solution
+    The input aircraft model, which is already sized, will be use a initial guess
+    Mission requirements, including range and fuel will be overwritting the model
+    Optimization bounds will be limiting the 15 parameters to be tuned
+    6 constraints will be ensured satisfied by the end of optimization
+Inputs:
+    ac: TASOPT.aircraft: Aircraft model to be optimized and used for initialization (Already sized)
+    mission_req: MissionReq: Required range and fuel
+    bounds_opt: BoundsOpt: 15 parameters to tunes and their absolute bounds [AR,CL,sweep(deg),altitude,λ_in,λ_out,t/c_root,t/c_span,rcls,rclt,Tt4,π_hc,π_f,π_lc,BPR]
+    constraints_opt: ConstraintsOpt: 6 constraints for optimization [max_span,max_lenField,min_TOCGamma,max_Tt3,max_TMetal,max_DiaFan]
+    tol_rel:Float64: Relative tolerance for the optmizer to converge
+    iters_max_opt:Int: maximum iteration for the optimizers to run
+    optimizer_type:Symbol: optimization method
+Outputs:
+    hist_optim: OptHistory: Optimization history [test_param,penalty,PFEI,violations]
+    status: Symbol: status of the optimization [:FTOL_REACHED(Good), :MAXEVAL_REACHED(Okay), :FAILURE(Bad)]
+Updates:
+    ac
 """
-function optimize_aircraft!(
-    ac;
-    mission_req::NamedTuple,
-    constraints::AbstractVector{<:Real},
-    lower::AbstractVector{<:Real},
-    upper::AbstractVector{<:Real},
-    init_dx::AbstractVector{<:Real},
-    save_dir::AbstractString="ModelSaved",
-    save_name::AbstractString="acOptimized_Cus2",
-    tol_rel::Real=1e-6,
-    maxeval::Integer=1000,
-    optimizer::Symbol=:LN_NELDERMEAD,
-)
+function optimize_rangefuel_fun!(
+    ac; 
+    mission_req::MissionReq = MissionReq(),
+    bounds_opt::BoundsOpt = BoundsOpt(),
+    constraints_opt::ConstraintsOpt = ConstraintsOpt(),
+    tol_rel::Float64=1e-6, iters_max_opt::Int=1000, 
+    optimizer_type::Symbol=:LN_NELDERMEAD)
 
-#### IO Prepare
-# Make a folder for saving optimized aircraft model
-save_dir = "ModelSaved"
-mkpath(save_dir)
+    #### Size check
+    # Model
+    ac.is_sized[1] || throw(ArgumentError("Input aircraft model has to be sized first"))
+    tol_rel > 0 || throw(ArgumentError("Relative tolerance for optimization must be > 0"))
+    iters_max_opt > 0 || throw(ArgumentError("maximum number of optimization interations must be > 0"))
+    # Mission requirements
+    mission_req.range_des > 0 || throw(ArgumentError("Design flight range > 0"))
+    mission_req.rho_fuel > 0 || throw(ArgumentError("Fuel density > 0"))
+    mission_req.hvap_fuel >= 0 || throw(ArgumentError("Fuel heat of vaporization >= 0"))
+    # Constraints
+    constraints_opt.span_max > 0 || throw(ArgumentError("span_max > 0")) # Maximum span (m)
+    constraints_opt.lenField_max > 0 || throw(ArgumentError("lenField_max > 0")) # Maximum balanced field length (m)
+    constraints_opt.TOCGamma_min > 0 || throw(ArgumentError("TOCGamma_min > 0")) # Minimum top-of-climb flight angle (rad)
+    constraints_opt.Tt3_max > 0 || throw(ArgumentError("Tt3_max > 0")) # Maximum combustor inlet temperature (K)
+    constraints_opt.TMetal_max > 0 || throw(ArgumentError("TMetal_max > 0")) # Maximum metal temperature (K)
+    constraints_opt.DiaFan_max > 0 || throw(ArgumentError("DiaFan_max > 0")) # Maximum fan diameter (m)
 
-# Make a save name for the optimized model
-save_Name = "acOptimized_Cus2"
+    #### Modify the mission requirement for optimization
+    ac.parm[imRange,:] .= mission_req.range_des #Design flight range (m)
+    ac.options.ifuel = mission_req.idx_fuel #Fuel Index
+    ac.parg[igrhofuel] = mission_req.rho_fuel #Fuel Density (kg/m3)
+    ac.pare[iehvap, :, :] .= mission_req.hvap_fuel #Heat of Vaporization (J/kg)
+    ac.pare[iehvapcombustor, :, :] .= mission_req.hvap_fuel
 
-#### Loading an Baseline Aircraft Model
-ac = read_aircraft_model("./customized/narrow_input.toml"; templatefile = "./customized/narrow_input.toml")
-size_aircraft!(ac)
-# ##### Or if load an already sized mission
-# ac = quickload_aircraft(joinpath(save_dir,"acOptimized_Cus.jld2"))
+    #### Create an objective function
+    # Initialize a history of optimization
+    hist_optim = OptHistory() #Optimization history
+    # Setup the constraints
+    constraints_optim = [constraints_opt.span_max, constraints_opt.lenField_max, constraints_opt.TOCGamma_min,
+    constraints_opt.Tt3_max, constraints_opt.TMetal_max, constraints_opt.DiaFan_max]
+    # Get an objective function
+    obj = make_obj(ac, constraints_optim, hist_optim) #This obj modify the aircraft model in place
 
-#### Modify the mission requirement for optimization
-ac.parm[imRange,:] .= 3000.0 * 1852.0 #Size at different flight range (m)
-ac.options.ifuel = 24 #Fuel Index: Jet Fuel(24), Ethanol(32)
-ac.parg[igrhofuel] = 817.0 #Fuel Density: Jet Fuel(817.0) (kg/m3), Ethanol(789.0) (kg/m3)
-ac.pare[iehvap, :, :] .= 358694.0 #Heat of Vaporization: Jet Fuel(358694.0) (J/kg), Ethanol(918187.9) (J/kg)
-ac.pare[iehvapcombustor, :, :] .= 358694.0
-
-#### Get the objective function for optimization
-hist_optim = OptHistory() #Optimization history
-# Setup constraints
-max_span = 35.814 #Maximum Span(m)
-max_lenField = 2400.0 #Maximum Balanced Field Length(m)
-min_TOCGamma = 0.015 #Minimum Top of Climb Flight Angle (rad)
-max_Tt3 = 900.0 #Maximum Combustor Inlet Temperature (K)
-max_TMetal = 1333.33 #Maximum Metal Tempeature (K)
-max_DiaFan = 2.0 #Maximum Fan Diameter (m)
-constraints_optim = [max_span, max_lenField, min_TOCGamma, max_Tt3, max_TMetal, max_DiaFan]
-# Get objective function
-obj = make_obj(ac, constraints_optim, hist_optim)
-
-#### Setup the optimized parameters
-""" These are the parameters to optimize
-       x: [AR,    CL,  sweep(deg), altitude, λ_in,  λ_out, t/c_root, t/c_span, rcls, rclt,   Tt4,  π_hc,   π_f,   π_lc,   BPR]
-            1.    2.    3.          4.         5.     6.     7.         8.       9.   10.     11.    12.    13.    14.     15.
-""" 
-lower   = [6.0,  0.45, 25.0,      10000.0,    0.65,  0.1,   0.125,    0.125,    0.9,  0.7,  1400.0, 10.0,  1.25,   2.999,   1.0]
-upper   = [18.0, 0.75, 30.0,      20000.0,    0.85,  0.4,   0.15,     0.15,     1.3,  1.0,  1650.0, 15.0,  2.0,    3.001,  20.0]
-init_dx = [0.5,  0.05,  0.1,       200.0,     0.01,  0.01,  0.01,     0.01,     0.01, 0.01, 100.0,   0.5,  0.05,   0.0001  ,1.0]
-initial = [
-    ac.wing.layout.AR,                                     # 1.  Current aspect ratio
-    ac.para[iaCL, ipcruise1, 1],                           # 2.  Current cruise CL
-    ac.wing.layout.sweep,                                  # 3.  Current sweep angle (deg)
-    ac.para[iaalt, ipcruise1, 1],                          # 4.  Current cruise altitude
-    ac.wing.inboard.λ,                                     # 5.  Current inner taper ratio
-    ac.wing.outboard.λ,                                    # 6.  Current outer taper ratio
-    ac.wing.inboard.cross_section.thickness_to_chord,      # 7.  Current root t/c
-    ac.wing.outboard.cross_section.thickness_to_chord,     # 8.  Current span t/c
-    ac.para[iarcls, ipcruise1, 1],                         # 9.  Current rcls (Cl ratios)
-    ac.para[iarclt, ipcruise1, 1],                         # 10. Current rclt
-    ac.pare[ieTt4, ipcruise1, 1],                          # 11. Current Tt4
-    ac.pare[iepihc, ipcruise1, 1],                         # 12. Current π_hc
-    ac.pare[iepif, ipcruise1, 1],                          # 13. Current π_f
-    ac.pare[iepilc, ipcruise1, 1],                         # 14. Current π_lc
-    ac.pare[ieBPR, ipcruise1, 1],                          # 15. Current BPR
-]
-# Filter the initial conditions
-for i in 1:length(initial)
-    if initial[i]>upper[i] || initial[i]<lower[i]
-        initial[i] = 0.5*(upper[i]+lower[i])
+    #### Setup the optimized parameters
+    # Construct the bounds
+    numBounds = fieldcount(BoundsOpt)
+    lower   = Vector{Float64}(undef, numBounds)
+    upper   = Vector{Float64}(undef, numBounds)
+    init_dx = Vector{Float64}(undef, numBounds)
+    for (idx, fNam) in enumerate(fieldnames(BoundsOpt))
+        lower_cur, upper_cur, init_dx_cur = getfield(bounds_opt, fNam)
+        # Check error
+        lower_cur < upper_cur || throw(ArgumentError("Optimization parameters: $(fNam): lower bound must be < upper bound"))
+        init_dx_cur > 0  || throw(ArgumentError("Optimization initial step for: $(fNam): must be > 0"))
+        # Form vector
+        lower[idx]   = lower_cur
+        upper[idx]   = upper_cur
+        init_dx[idx] = init_dx_cur
     end
-end
-# set tolerances
-tol_rel = 1e-6
-iters_max_opt = 1000
+    # Use the initial conditions from aircraft model (Reused previous optimized solution)
+    initial = [
+    ac.wing.layout.AR,                                     #1  Wing aspect ratio
+    ac.para[iaCL, ipcruise1, 1],                           #2  Cruise CL
+    ac.wing.layout.sweep,                                  #3  Wing sweep angle (deg)
+    ac.para[iaalt, ipcruise1, 1],                          #4  Cruise altitude (m)
+    ac.wing.inboard.λ,                                     #5  Inboard wing taper ratio
+    ac.wing.outboard.λ,                                    #6  Outboard wing taper ratio
+    ac.wing.inboard.cross_section.thickness_to_chord,      #7  Inboard thickness-to-chord ratio
+    ac.wing.outboard.cross_section.thickness_to_chord,     #8  Outboard thickness-to-chord ratio
+    ac.para[iarcls, ipcruise1, 1],                         #9  Break/root Cl ratio at cruise
+    ac.para[iarclt, ipcruise1, 1],                         #10 Tip/root Cl ratio at cruise
+    ac.pare[ieTt4, ipcruise1, 1],                          #11 Tt4 at cruise (K)
+    ac.pare[iepihc, ipcruise1, 1],                         #12 HPC pressure ratio at cruise
+    ac.pare[iepif, ipcruise1, 1],                          #13 Fan pressure ratio at cruise
+    ac.pare[iepilc, ipcruise1, 1],                         #14 LPC pressure ratio at cruise
+    ac.pare[ieBPR, ipcruise1, 1],                          #15 Bypass ratio at cruise
+    ]
+    # Filter the out of bound initial conditions
+    initial_saved = initial #save an unclamped initial to retract back the aircraft model state
+    initial = clamp.(initial, lower, upper)
 
-#### Setup and run optimization
-# Set the optimizer
-opt = NLopt.Opt(:LN_NELDERMEAD, length(initial))
-opt.lower_bounds  = lower
-opt.upper_bounds  = upper
-opt.min_objective = obj
-opt.initial_step  = init_dx
-opt.ftol_rel      = tol_rel
-opt.maxeval       = iters_max_opt
+    #### Setup the optimizer
+    opt = NLopt.Opt(optimizer_type, numBounds)
+    opt.lower_bounds  = lower
+    opt.upper_bounds  = upper
+    opt.min_objective = obj
+    opt.initial_step  = init_dx
+    opt.ftol_rel      = tol_rel
+    opt.maxeval       = iters_max_opt
 
-# Optimize
-opt_time = @elapsed begin
+    #### Run the optimization
     try
-        (optf, optx, ret) = NLopt.optimize(opt, initial)
-    catch e
-        println("Optimization failed: $e")
-        optf, optx, ret = Inf, initial, :FAILURE
+        (_, _, status) = NLopt.optimize(opt, initial)
+    catch errOpt
+        println("Current O=optimization failed: $(errOpt)")
+        (_, _, status) = (Inf, initial, :FAILURE)
     end
+
+    #### Prepare an updated aircraft model state
+    if status != :FAILURE
+        bestSol = best_feasible(hist_optim)
+        if isnothing(bestSol)
+            println("No constraint satisfying solutions found, return to starting state")
+            obj(initial_saved, Float64[]) #This likely wont be feasible either
+        else
+            println("Identified and setup the current best solution with optimization reached by $(status)")
+            obj(bestSol.test_param, Float64[])
+        end
+    else
+        println("Optimization process failed, return to starting state")
+        obj(initial_saved, Float64[]) #This likely wont be feasible either
+    end
+
+    return status, hist_optim
 end
 
-# Select the best solution that satisfy all the constraints
-bestSol = best_feasible(hist_optim)
-if isnothing(bestSol)
-    println("No feasible solution that satisfy the constraints found")
-else
-    println("Identified and ran the best solution")
-    penalty = obj(bestSol.test_param, Float64[])
-    quicksave_aircraft(ac,joinpath(save_dir, "$(save_Name).jld2"))
-end
+end # module OptimizeRangeFuel
