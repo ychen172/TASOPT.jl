7129d69081b71b9c40728351924738516333dd8b
yang chen <yangchen@dhcp-10-29-213-176.dyn.MIT.EDU>
2026-05-12 16:03:56 -0400

    Collect the design parameters for all cases

--- example/example_postprocess.jl
+++ example/example_postprocess.jl
@@ -1,31 +1,48 @@
 """
-This script extract parameters and print out for designed mission
+This script compare the design point performance between ethanol and jet
+Fixed design bounds and Fixed constraints across different cases assumed
 """
 
-# 1) Load TASOPT
 using TASOPT
 using DataFrames, CSV
-using Plots
 include(__TASOPTindices__)
 include(joinpath(@__DIR__, "postprocess.jl"))
 using .PostProcess: ExtractDes
 using .PostProcess.OptimizeRangeFuel: BoundsOpt, ConstraintsOpt
 
-####Setup IO
-load_dir = "ModelSaved"
-load_name = "acOptimized_BatOptEth400"
-save_dir = "ModelProcessed"
-save_name = "BatOptEth400"
-mkpath(load_dir)
+#### Setup IO
+model_dir   = "ModelSaved"
+model_name  = "acOptimized_BatOpt" #Frontal key name for the models(FuelRange)
+save_dir    = "ModelProcessed" #Outer Directory for saving models
+save_name   = "EthVsJet" #The total save name for jet and ethanol comparison
+mkpath(model_dir)
 mkpath(save_dir)
+save_dirSub = joinpath(save_dir,save_name) #Sub-directory to save comparison data
+mkpath(save_dirSub)
 
-####Load model
-ac = quickload_aircraft(joinpath(load_dir,"$(load_name).jld2"))
+#### Setup parameters
+Fuels = ["Eth","Jet"]
+Ranges = collect(300:100:3000)
+# flg_save = false #Whether to save individual model outputs
 
-####Setup constraints and search bound (Need to be saved by optimization later)
-bounds_opt = BoundsOpt()
-constraints_opt = ConstraintsOpt() #Current setup use default values
+#### Extract data
+desParamRows = [] #Each row is one case
+for (i,curFuel) in enumerate(Fuels)
+    for (j,curRange) in enumerate(Ranges)
+        case_id = "$(curFuel)$(curRange)" #Current Case
+        model_file = "$(model_name)$(case_id).jld2"
+        println("Reading case: Fuel: $(curFuel), Range: $(curRange)")
+        # Try to load and extract the design parameters
+        try
+            ac = quickload_aircraft(joinpath(model_dir,model_file))
+            designParam = ExtractDes(ac, save_dir, case_id; flg_save=false)
+            push!(desParamRows, (; case = case_id, designParam...))
+        catch err
+            println("$(model_file) not readable")
+        end
+    end
+end
 
-####Extract and save the performance data
-designParam = ExtractDes(ac, save_dir, save_name; flg_save=true, bounds_opt=bounds_opt, constraints_opt=constraints_opt)
-println(designParam)
+#### Save the comparison design parameters
+df_desPara = DataFrame(desParamRows)
+CSV.write(joinpath(save_dirSub, "$(save_name)_DesPara.csv"), df_desPara)
