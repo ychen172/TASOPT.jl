c43e6c432032e190cf518b67b459d3d96126073b
yang chen <yangchen@dhcp-10-29-213-176.dyn.mit.edu>
2026-05-13 18:19:03 -0400

    Get the data out from example cases

--- example/example_postprocess_plot.jl
+++ example/example_postprocess_plot.jl
@@ -1,6 +1,7 @@
 """
 This script compare the design point performance between ethanol and jet
-Fixed design bounds and Fixed constraints across different cases assumed
+Adaptive design parameter bounds across different cases
+Fixed and default constraints across different cases
 """
 
 using TASOPT
@@ -9,61 +10,62 @@ include(__TASOPTindices__)
 include(joinpath(@__DIR__, "postprocess.jl"))
 using .PostProcess: ExtractDes
 using .PostProcess.OptimizeRangeFuel: BoundsOpt, ConstraintsOpt
+include(joinpath(@__DIR__, "optimize_rangefuel.jl"))
+using .OptimizeRangeFuel.extract_opt_para
 
 #### Setup IO
-model_dir   = "ModelSaved"
-model_name  = "acOptimized_BatOpt" #Frontal key name for the models(FuelRange)
-save_dir    = "ModelProcessed" #Outer Directory for saving models
-save_name   = "EthVsJet" #The total save name for jet and ethanol comparison
+model_dir    = "ModelSavedTest"
+model_prefix = "acOptimized_BatOpt" #Frontal key name for the models(FuelRange)
+save_dir     = "ModelProcessed" #Outer Directory for saving models
+save_prefix  = "Eth_Jet_Comparison" #The total save name for jet and ethanol comparison
 mkpath(model_dir)
 mkpath(save_dir)
-save_dirSub = joinpath(save_dir,save_name) #Sub-directory to save comparison data
+save_dirSub = joinpath(save_dir,save_prefix) #Sub-directory to save comparison data
 mkpath(save_dirSub)
 
-#### Setup parameters
-Fuels = ["Eth","Jet"]
-Ranges = collect(300:100:3000)
-# flg_save = false #Whether to save individual model outputs
+#### Missions Parameters to Compare
+Fuels = ["Eth","Jet"] #These corresponding to the model file name
+Ranges = collect(300:100:3000) #Prefixex+Fuel+Range.jld2
+flgSaveIndividual = false #Whether to save an output for individual case
 
-#### Extract data
-desParamRows = [] #Each row is one case
-for (i,curFuel) in enumerate(Fuels)
-    for (j,curRange) in enumerate(Ranges)
-        case_id = "$(curFuel)$(curRange)" #Current Case
-        model_file = "$(model_name)$(case_id).jld2"
-        println("Reading case: Fuel: $(curFuel), Range: $(curRange)")
-        # Try to load and extract the design parameters
-        try
-            ac = quickload_aircraft(joinpath(model_dir,model_file))
-            designParam = ExtractDes(ac, save_dir, case_id; flg_save=false)
-            push!(desParamRows, (; case = case_id, designParam...))
-        catch err
-            println("$(model_file) not readable")
-        end
-    end
-end
-
-#### Save the comparison design parameters
-df_desPara = DataFrame(desParamRows)
-CSV.write(joinpath(save_dirSub, "$(save_name)_DesPara.csv"), df_desPara)
-
-#### Also save the fixed constraints and fixed optimization bounds
-bounds_opt = BoundsOpt()
+#### Load the default constraints which is believed at this point to be common to call cases
 constraints_opt = ConstraintsOpt()
-# saving
-function save_struct(datStr,filename)
-    numfields = fieldcount(typeof(datStr))
-    lenfields = length(getfield(datStr,1))
-    namfields = collect(fieldnames(typeof(datStr)))
-    data = Matrix{Float64}(undef, lenfields, numfields)
-    for i = 1:numfields
-        for j = 1:lenfields
-            data[j,i] = getfield(datStr,i)[j]
+
+#### Extract data for each case
+paraSet = [] # Each element is one fuel
+boundSet = [] # Each element is one fuel
+optParSet = []
+for (i,curFuel) in enumerate(Fuels)
+    paraSetSub = [] # Each element is one range
+    boundSetSub = [] # Each element is one range
+    optParSetSub = []
+    for (j,curRange) in enumerate(Ranges)
+        caseCur = "$(curFuel)$(round(Int,curRange))"
+        println("Attempt to read case: Fuel type: $(curFuel), Range: $(curRange)")
+        try
+            # Load the aircraft model
+            ac = quickload_aircraft(joinpath(model_dir,"$(model_prefix)$(caseCur).jld2"))
+            # Load the parameter bounds
+            bdcsv = CSV.read(joinpath(model_dir,"$(model_prefix)$(caseCur)_BoundLocal.csv"), DataFrame)
+            bounds_opt = BoundsOpt()
+            for fName in fieldnames(typeof(bounds_opt))
+                fNameSym = Symbol(fName)
+                setfield!(bounds_opt, fName, (Float64(bdcsv[1, fNameSym]), Float64(bdcsv[2, fNameSym]), Float64(bdcsv[3, fNameSym])))
+            end
+            # Extract the design parameters and optionally save individual case
+            design_para = ExtractDes(ac, save_dir, "$(model_prefix)$(caseCur)"; flg_save=flgSaveIndividual,
+                          bounds_opt=bounds_opt, constraints_opt=constraints_opt)
+            # Extract the subset of design parameters that have bounds corresponds to
+            optimi_para = extract_opt_para(ac) #vector{Float64} Those parameters having bounds. Same order as the bound
+            # Append the data
+            push!(paraSetSub, (; range_id = string(round(Int,curRange)), fuel_id = curFuel, design_para...))
+            push!(boundSetSub, (bounds_opt))
+            push!(optParSetSub, (optimi_para))
+        catch err
+            println("Reading case failed for Fuel type: $(curFuel), Range: $(curRange)")
         end
     end
-    df = DataFrame(data, Symbol.(namfields))
-    CSV.write(filename, df)
-    return df
+    push!(paraSet, (paraSetSub))
+    push!(boundSet, (boundSetSub))
+    push!(optParSet, (optParSetSub))
 end
-save_struct(bounds_opt, joinpath(save_dirSub, "$(save_name)_OptBounds.csv"))
-save_struct(constraints_opt, joinpath(save_dirSub, "$(save_name)_Constraints.csv"))
