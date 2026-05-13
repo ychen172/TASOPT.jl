1c00662386e4fc816914b8c549d96056366e612a
yang chen <yangchen@yangs-MacBook-Pro.local>
2026-05-13 16:20:55 -0400

    Add a single file post-processing script

--- example/example_postprocess_single.jl
+++ example/example_postprocess_single.jl
@@ -11,59 +11,31 @@ using .PostProcess: ExtractDes
 using .PostProcess.OptimizeRangeFuel: BoundsOpt, ConstraintsOpt
 
 #### Setup IO
+# I
 model_dir   = "ModelSaved"
-model_name  = "acOptimized_BatOpt" #Frontal key name for the models(FuelRange)
+model_name  = "acOptimized_BatOptJet1500" #Frontal key name for the models(FuelRange)
+bounds_name = "acOptimized_BatOptJet2800_BoundLocal" #.csv the bounds for the optimized parameters
+# O
 save_dir    = "ModelProcessed" #Outer Directory for saving models
-save_name   = "EthVsJet" #The total save name for jet and ethanol comparison
+save_name   = "BatOptJet1500" #The total save name for jet and ethanol comparison
 mkpath(model_dir)
 mkpath(save_dir)
 save_dirSub = joinpath(save_dir,save_name) #Sub-directory to save comparison data
 mkpath(save_dirSub)
 
-#### Setup parameters
-Fuels = ["Eth","Jet"]
-Ranges = collect(300:100:3000)
-# flg_save = false #Whether to save individual model outputs
-
-#### Extract data
-desParamRows = [] #Each row is one case
-for (i,curFuel) in enumerate(Fuels)
-    for (j,curRange) in enumerate(Ranges)
-        case_id = "$(curFuel)$(curRange)" #Current Case
-        model_file = "$(model_name)$(case_id).jld2"
-        println("Reading case: Fuel: $(curFuel), Range: $(curRange)")
-        # Try to load and extract the design parameters
-        try
-            ac = quickload_aircraft(joinpath(model_dir,model_file))
-            designParam = ExtractDes(ac, save_dir, case_id; flg_save=false)
-            push!(desParamRows, (; case = case_id, designParam...))
-        catch err
-            println("$(model_file) not readable")
-        end
-    end
-end
-
-#### Save the comparison design parameters
-df_desPara = DataFrame(desParamRows)
-CSV.write(joinpath(save_dirSub, "$(save_name)_DesPara.csv"), df_desPara)
-
-#### Also save the fixed constraints and fixed optimization bounds
-bounds_opt = BoundsOpt()
+#### Assumed default constraints used
 constraints_opt = ConstraintsOpt()
-# saving
-function save_struct(datStr,filename)
-    numfields = fieldcount(typeof(datStr))
-    lenfields = length(getfield(datStr,1))
-    namfields = collect(fieldnames(typeof(datStr)))
-    data = Matrix{Float64}(undef, lenfields, numfields)
-    for i = 1:numfields
-        for j = 1:lenfields
-            data[j,i] = getfield(datStr,i)[j]
-        end
-    end
-    df = DataFrame(data, Symbol.(namfields))
-    CSV.write(filename, df)
-    return df
+
+#### Loading the bounds for optimization
+bounds_opt = BoundsOpt()
+df = CSV.read(joinpath(model_dir,"$(bounds_name).csv"), DataFrame)
+fNames = fieldnames(BoundsOpt)
+for fNameCur in fNames
+    col = Symbol(fNameCur)
+    vals = (Float64(df[1, col]), Float64(df[2, col]), Float64(df[3, col]))
+    setfield!(bounds_opt, fNameCur, vals)
 end
-save_struct(bounds_opt, joinpath(save_dirSub, "$(save_name)_OptBounds.csv"))
-save_struct(constraints_opt, joinpath(save_dirSub, "$(save_name)_Constraints.csv"))
+
+#### Setup parameters
+ac = quickload_aircraft(joinpath(model_dir,"$(model_name).jld2"))
+designParam = ExtractDes(ac, save_dirSub, save_name; flg_save=true, bounds_opt=bounds_opt, constraints_opt=constraints_opt)
