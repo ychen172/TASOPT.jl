5e2326a07a7851de6c9dc624d05ee35ad3048954
yang chen <yangchen@dhcp-10-29-207-55.dyn.MIT.EDU>
2026-05-21 12:27:55 -0400

    Plot payload

--- example/example_compare_retrofit.jl
+++ example/example_compare_retrofit.jl
@@ -27,17 +27,20 @@ mkpath(save_dir_sub)
 #### Initialization
 range_lst = [] #(nmi)
 PFEI_lst = [] #(J/J)
+massEmp_lst = [] #(Ton)
 
 #### Extract data for each scenerio
 for (i, keyword_cur) in enumerate(case_keywords)
     model_dir_sub = joinpath(model_dir, keyword_cur)
     range_lst_sub = [] #(nmi)
     PFEI_lst_sub = [] #(J/J)
+    massEmp_lst_sub = [] #(Ton)
     # Extract data for each design case
     for (j, des_range_cur) in enumerate(des_ranges)
         model_dir_sub_sub = joinpath(model_dir_sub, keyword_cur*"$(round(Int,des_range_cur))")
         range_lst_sub_sub = [] #(nmi)
         PFEI_lst_sub_sub = [] #(J/J)
+        massEmp_lst_sub_sub = [] #(Ton)
         ## extract for each off-design case
         for (k, offdes_range_cur) in enumerate(offdes_ranges)
             model_dir_sub_sub_sub = joinpath(model_dir_sub_sub, keyword_cur*"$(round(Int,des_range_cur))_$(round(Int,offdes_range_cur)).jld2")
@@ -48,17 +51,28 @@ for (i, keyword_cur) in enumerate(case_keywords)
                 ## extract data
                 range_cur = ac_cur.parm[imRange,2] / 1852.0 #(nmi)
                 PFEI_cur = ac_cur.parm[imPFEI, 2] #(J/J)
+                
+                ## Check on masses
+                massTO = ac_cur.parm[imWTO,2]/gee/1000.0 #Takeoff mass (Ton)
+                massFuelTot = ac_cur.parm[imWfuel,2]/gee/1000.0 #Fuel mass (Ton)(Include reserved and burned)
+                massPayload = ac_cur.parm[imWpay, 2]/gee/1000.0 #(Ton)
+                massEmpty = massTO - massFuelTot - massPayload #(Ton) empty weight
+                
+                ## store
                 push!(range_lst_sub_sub, range_cur)
                 push!(PFEI_lst_sub_sub, PFEI_cur)
+                push!(massEmp_lst_sub_sub, massEmpty)
             catch
                 nothing
             end
         end
         push!(range_lst_sub,range_lst_sub_sub)
         push!(PFEI_lst_sub,PFEI_lst_sub_sub)
+        push!(massEmp_lst_sub,massEmp_lst_sub_sub)
     end
     push!(range_lst,range_lst_sub)
     push!(PFEI_lst,PFEI_lst_sub)
+    push!(massEmp_lst,massEmp_lst_sub)
 end
 
 #### Plotting
@@ -71,3 +85,13 @@ for (i, keyword_cur) in enumerate(case_keywords)
     end
 end
 savefig(plot_PFEI, joinpath(save_dir_sub, "PFEI_comparison.png"))
+
+# Empty weight
+plot_mEmpty = plot(xlabel="Range (nmi)", ylabel="Empty Mass (Ton)", dpi=800)
+plot!(plot_mEmpty, ylims=(0.0, massEmp_lst[1][1][1]+30.0))
+for (i, keyword_cur) in enumerate(case_keywords)
+    for (j, des_range_cur) in enumerate(des_ranges)
+        plot!(plot_mEmpty, range_lst[i][j], massEmp_lst[i][j], marker=:cross, lw=2, label=case_names[i]*"_$(round(Int,des_ranges[j]))")
+    end
+end
+savefig(plot_mEmpty, joinpath(save_dir_sub, "mass_empty_comparison.png"))
