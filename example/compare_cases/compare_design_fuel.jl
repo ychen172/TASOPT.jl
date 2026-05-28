156ae83afbe4481c73386c0c3c31aefdbc575d7c
yang chen <yangchen@dhcp-10-29-207-55.dyn.mit.edu>
2026-05-28 12:05:59 -0400

    Get a design point comparison

--- example/compare_cases/compare_design_fuel.jl
+++ example/compare_cases/compare_design_fuel.jl
@@ -133,6 +133,8 @@ function extract_acModel(ac_cur,idx_miss)
     #### fuel volume data
     rhoFuel = ac_cur.parg[igrhofuel] #kg/m3
     volFuel = massFuelTot * 1000.0 / rhoFuel #m3
+    volFuelMax = ac_cur.parg[igWfmax] / gee / rhoFuel #Only work if design and off-design use the same fuel. Fuel volume maximum m3
+    FuelVolumeFraction = volFuel/volFuelMax
     
     #### flight performance data
     LD_cruise = 0.5 * (ac_cur.para[iaCL, ipcruise1, idx_miss]/ac_cur.para[iaCD, ipcruise1, idx_miss] + 
@@ -215,7 +217,8 @@ function extract_acModel(ac_cur,idx_miss)
         "spe_power_cru_Jkg" => spePower_cru,
         "eta_propu_cru" => etaPropu_cru,
         "OPR_cru" => OPR_cru,
-        "Tt_turbin_cru_K" => Tt_TurbIn_cru
+        "Tt_turbin_cru_K" => Tt_TurbIn_cru,
+        "FuelVolumeFraction"=> FuelVolumeFraction
     )
     return output
 end
@@ -248,87 +251,53 @@ end
 #### Setup IO
 # Input case names - Retrofit
 model_dir       = "../ModelSaved"
-key_retro       = "jetfuel_to_ethanolJet"
-des_range_retro = [3000] #float.(collect(300:100:3000)) #design range for retrofit aircraft to compare (Has to be convertable to integer) (nmi)
-OD_range_retro  = float.(collect(300:100:8000)) #Search set for off-design ranges (Has to be integer) (nmi) (can be wider than existing)
-# Input case names - Sized
-key_sized       = "OptimizedJetToEth3000_" #180Pass_Opt: "acOptimized_BatOptEth", 3000nmiJet2EthRetroCase_but_optimized: "OptimizedJetToEth3000_"
-des_range_sized = float.(collect(300:100:2100)) #Has to match with existings
-# Input case names - Sized with Central Fuel Tank
-key_sized_C       = "OptCenTankJetToEth3000_" #180Pass_Opt: "CenterFuelTank_BatOptEth", 3000nmiJet2EthRetroCase_but_optimizedWithCenTank: "OptCenTankJetToEth3000_"
-des_range_sized_C = float.(collect(300:100:2100)) #Has to match with existings
+# Input case names - Ethanol
+key_sized_Eth       = "acOptimized_BatOptEth" #180Pass_Opt: "acOptimized_BatOptEth", 3000nmiJet2EthRetroCase_but_optimized: "OptimizedJetToEth3000_"
+des_range_sized_Eth = float.(collect(300:100:2900)) #Has to match with existings
+# Input case names - Jet fuel
+key_sized_Jet       = "acOptimized_BatOptJet" #180Pass_Opt: "CenterFuelTank_BatOptEth", 3000nmiJet2EthRetroCase_but_optimizedWithCenTank: "OptCenTankJetToEth3000_"
+des_range_sized_Jet = float.(collect(300:100:3000)) #Has to match with existings
 # Output directory
 save_dir      = "../ModelProcessed"
-save_name     = "Compare_Retrofit_Sized" #sub_folder will be created
+save_name     = "Compare_at_design" #sub_folder will be created
 # Fields to read out
 const fields = (:range_nmi,   :PFEI_JJ, :massTO_Ton, :massFuelTot_Ton, :massPayload_Ton,
                 :massEmpty_Ton, :rhoFuel_kgm3,  :volFuel_m3, :LD_cru, :LHV_cru_Jkg, :TSFC_cru_kgsN,
                 :vel_cru_ms, :eta_total_cru, :ene_fli_J, :frac_rese, :PFEI_cru_JJ, :range_cru_m,
-                :eta_therm_cru, :spe_power_cru_Jkg, :eta_propu_cru, :OPR_cru, :Tt_turbin_cru_K)
+                :eta_therm_cru, :spe_power_cru_Jkg, :eta_propu_cru, :OPR_cru, :Tt_turbin_cru_K, :FuelVolumeFraction)
 
 #### Create save directory
 save_dir_sub  = joinpath(save_dir,save_name)
 mkpath(save_dir_sub)
 
 #### Initialization
-results_retro = init_results_3Layers(length(des_range_retro), fields, length(OD_range_retro))
-results_sized = init_results_2Layers(length(des_range_sized), fields)
-results_sized_C = init_results_2Layers(length(des_range_sized_C), fields)
+results_sized_Eth = init_results_2Layers(length(des_range_sized_Eth), fields)
+results_sized_Jet = init_results_2Layers(length(des_range_sized_Jet), fields)
 
-#### Extract data for the retrofit missions
-for (i, des_ranges_cur) in enumerate(des_range_retro)
-    j = 0 #valid off-design index
-    results_retro_cur = results_retro[i]
-    for offdes_ranges_cur in OD_range_retro
-        ####Reading out the missions data    
-        ac_dir_retro = joinpath(model_dir,key_retro,key_retro*"$(round(Int,des_ranges_cur))",key_retro*"$(round(Int,des_ranges_cur))_$(round(Int,offdes_ranges_cur)).jld2")
-        if !isfile(ac_dir_retro) #check if the file exist
-            println("File, $(ac_dir_retro), does not exist")
-            continue
-        end
-        ac_retro = quickload_aircraft(ac_dir_retro)
-        println("File, $(ac_dir_retro), read successfully")
-        
-        #### Extract important parameters
-        out_dict = extract_acModel(ac_retro,2)
-
-        #### Output the data
-        j += 1
-        for f in fields
-            results_retro_cur[f][j] = out_dict[String(f)]
-        end
-    end
-
-    #### Trim the trailing missing
-    for f in fields
-        resize!(results_retro_cur[f], j)
-    end
-end
-    
 #### Extract data for the sized missions
-for (i, des_ranges_cur) in enumerate(des_range_sized)
+for (i, des_ranges_cur) in enumerate(des_range_sized_Eth)
     #### Read in the case
-    ac_dir_sized = joinpath(model_dir,key_sized,key_sized*"$(round(Int,des_ranges_cur)).jld2")
+    ac_dir_sized = joinpath(model_dir,key_sized_Eth,key_sized_Eth*"$(round(Int,des_ranges_cur)).jld2")
     ac_sized = quickload_aircraft(ac_dir_sized)
     println("File, $(ac_dir_sized), read successfully")
     out_dict = extract_acModel(ac_sized,1)
     
     #### Output the data
     for f in fields
-        results_sized[f][i] = out_dict[String(f)]
+        results_sized_Eth[f][i] = out_dict[String(f)]
     end
 end
 
-for (i, des_ranges_cur) in enumerate(des_range_sized_C)
+for (i, des_ranges_cur) in enumerate(des_range_sized_Jet)
     #### Read in the case
-    ac_dir_sized_C = joinpath(model_dir,key_sized_C,key_sized_C*"$(round(Int,des_ranges_cur)).jld2")
-    ac_sized_C = quickload_aircraft(ac_dir_sized_C)
-    println("File, $(ac_dir_sized_C), read successfully")
-    out_dict_C = extract_acModel(ac_sized_C,1)
+    ac_dir_sized = joinpath(model_dir,key_sized_Jet,key_sized_Jet*"$(round(Int,des_ranges_cur)).jld2")
+    ac_sized = quickload_aircraft(ac_dir_sized)
+    println("File, $(ac_dir_sized), read successfully")
+    out_dict = extract_acModel(ac_sized,1)
     
     #### Output the data
     for f in fields
-        results_sized_C[f][i] = out_dict_C[String(f)]
+        results_sized_Jet[f][i] = out_dict[String(f)]
     end
 end
 
@@ -339,27 +308,18 @@ linecolors = repeat([:blue, :red, :green, :orange, :purple, :brown, :pink, :gray
                      :magenta, :teal, :navy, :maroon, :olive, :gold, :coral, :turquoise, :lime, :indigo], 1000) 
 markers = repeat([:rect, :circle, :diamond, :utriangle, :dtriangle],1000)
 # Plot PFEI
-p1_1 = plot(xlabel="Ranges (nmi)", ylabel="PFEI (J/J)", dpi=800)
+p1_1 = plot(xlabel="Design Ranges (nmi)", ylabel="PFEI (J/J)", dpi=800)
 plot!(p1_1,ylims=(0.6,1.4))
 global il = 1
-plot!(p1_1, results_sized[:range_nmi], results_sized[:PFEI_JJ], marker=markers[il], mc=linecolors[il], msc=linecolors[il], color=linecolors[il], lw=2, linestyle=linestyles[il], label="Optimized aircratft")
+plot!(p1_1, results_sized_Jet[:range_nmi], results_sized_Jet[:PFEI_JJ], marker=markers[il], mc=linecolors[il], msc=linecolors[il], color=linecolors[il], lw=2, linestyle=linestyles[il], label="Optimized Jet Fuel Aircraft")
 global il += 1
-plot!(p1_1, results_sized_C[:range_nmi], results_sized_C[:PFEI_JJ], marker=markers[il], mc=linecolors[il], msc=linecolors[il], color=linecolors[il], lw=2, linestyle=linestyles[il], label="Optimized aircratft with central fuel tank")
-for i in eachindex(des_range_retro)
-    global il += 1
-    results_retro_cur = results_retro[i]
-    plot!(p1_1, results_retro_cur[:range_nmi], results_retro_cur[:PFEI_JJ], marker=markers[il], mc=linecolors[il], msc=linecolors[il], color=linecolors[il], lw=2, linestyle=linestyles[il], label="Retrofit Aircraft with $(round(Int, des_range_retro[i])) nmi desing range")
-end
+plot!(p1_1, results_sized_Eth[:range_nmi], results_sized_Eth[:PFEI_JJ], marker=markers[il], mc=linecolors[il], msc=linecolors[il], color=linecolors[il], lw=2, linestyle=linestyles[il], label="Optimized Ethanol Aircraft")
 savefig(p1_1, joinpath(save_dir_sub, "PFEI.png"))
 
-p1_2 = plot(xlabel="Ranges (nmi)", ylabel="Payload Mass (Ton)", dpi=800)
+# Plot Fuel Fraction
+p1_2 = plot(xlabel="Design Ranges (nmi)", ylabel="Fuel Tank Used (%)", dpi=800)
 global il = 1
-plot!(p1_2, results_sized[:range_nmi], results_sized[:massPayload_Ton], marker=markers[il], mc=linecolors[il], msc=linecolors[il], color=linecolors[il], lw=2, linestyle=linestyles[il], label=label="Optimized aircratft")
+plot!(p1_2, results_sized_Jet[:range_nmi], results_sized_Jet[:FuelVolumeFraction] .* 100.0, marker=markers[il], mc=linecolors[il], msc=linecolors[il], color=linecolors[il], lw=2, linestyle=linestyles[il], label="Optimized Jet Fuel Aircraft")
 global il += 1
-plot!(p1_2, results_sized_C[:range_nmi], results_sized_C[:massPayload_Ton], marker=markers[il], mc=linecolors[il], msc=linecolors[il], color=linecolors[il], lw=2, linestyle=linestyles[il], label="Optimized aircratft with central fuel tank")
-for i in eachindex(des_range_retro)
-    global il += 1
-    results_retro_cur = results_retro[i]
-    plot!(p1_2, results_retro_cur[:range_nmi], results_retro_cur[:massPayload_Ton], marker=markers[il], mc=linecolors[il], msc=linecolors[il], color=linecolors[il], lw=2, linestyle=linestyles[il], label="Retrofit Aircraft with $(round(Int, des_range_retro[i])) nmi desing range")
-end
-savefig(p1_2, joinpath(save_dir_sub, "mass_pay.png"))
+plot!(p1_2, results_sized_Eth[:range_nmi], results_sized_Eth[:FuelVolumeFraction] .* 100.0, marker=markers[il], mc=linecolors[il], msc=linecolors[il], color=linecolors[il], lw=2, linestyle=linestyles[il], label="Optimized Ethanol Aircraft")
+savefig(p1_2, joinpath(save_dir_sub, "FuelTankUsed.png"))
