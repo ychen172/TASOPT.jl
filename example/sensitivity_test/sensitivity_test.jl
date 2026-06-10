4125ded48a9390ad365d613d60d8eb92f91e7d3b
yang chen <yangchen@dhcp-10-29-207-55.dyn.MIT.EDU>
2026-06-09 22:40:29 -0400

    Use CL to size horizontal and vertical tail

--- example/sensitivity_test/sensitivity_test.jl
+++ example/sensitivity_test/sensitivity_test.jl
@@ -7,6 +7,11 @@ include(joinpath(__TASOPTroot__,"utils","sensitivity.jl"))
 modelPath = joinpath(@__DIR__,"../ModelSaved/acOptim_BatJet_CT/acOptim_BatJet_CT3000.jld2")
 println(modelPath)
 ac = quickload_aircraft(modelPath)
+ac.htail.opt_sizing = TailSizing.CLmaxFwdCG
+ac.htail.CL_max_fwd_CG = -0.7
+ac.vtail.opt_sizing = TailSizing.OEI
+ac.parg[igCLveout] = 0.5
+
 input_params = [
     :(ac.wing.layout.ηs),                                     # Panel break eta location
     :(ac.parg[igxeng]),                                       # Engine axial location
@@ -25,7 +30,9 @@ input_params = [
     :(ac.pare[iepihc, ipcruise1, 1]),                         # HPC pressure ratio
     :(ac.pare[iepif, ipcruise1, 1]),                          # Fan pressure ratio
     :(ac.pare[iepilc, ipcruise1, 1]),                         # LPC pressure ratio
-    :(ac.pare[ieBPR, ipcruise1, 1])                           # Bypass ratio
+    :(ac.pare[ieBPR, ipcruise1, 1]),                          # Bypass ratio
+    :(ac.htail.CL_max_fwd_CG),                                # CL max horizontal tail
+    :(ac.parg[igCLveout])                                     # CL max vertical tail
 ]
 
 names_params = [
@@ -46,7 +53,9 @@ names_params = [
     "pi_hc",
     "pi_f",
     "pi_lc",
-    "BPR"
+    "BPR",
+    "CL_h",
+    "CL_v"
 ]
 sensitivityVector = get_sensitivity(input_params; model_state=ac, eps=1e-5, optimizer=false, f_out_fn=nothing, diff_scheme=:central, metric=:impact)
 println(sensitivityVector)
