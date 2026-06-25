22e8001b0ea35aed9421de58ac2bac7b6a332d22
yang chen <yangchen@dhcp-10-29-207-55.dyn.mit.edu>
2026-06-24 23:23:08 -0400

    Update some new variabel extracted by compare_cases

--- example/compare_cases/compare_cases.jl
+++ example/compare_cases/compare_cases.jl
@@ -11,15 +11,16 @@ using .Extract: extract_acModel, init_results_2Layers, fill_results!, plot_cases
 #### Setup IO
 # Input case names - Retrofit
 model_dir  = "../ModelSaved"
-caseKeys   = ["Ethanol_ACTPlane_Opt","Ethanol_NoACT_Opt","JetFuel_ACTPlane_Opt","JetFuel_NoACT_Opt"]
-caseNames  = ["Ethanol with ACT","Ethanol without ACT","Jet Fuel with ACT","Jet Fuel without ACT"]
+caseKeys   = ["Opti_Jet_NoACT_",   "Opti_Eth_NoACT_",  "Opti_Jet_Thin_NoACT_", "Opti_Eth_Dense_NoACT_"]
+caseNames  = ["Jet Fuel Baseline", "Ethanol Baseline", "Thinner Jet Fuel",     "Denser Ethanol"]
 idxMiss    = [1,1,1,1]
-ranges     = collect(300:100:2900) 
+ranges     = collect(300:100:3000) 
 # Output directory
 save_dir      = "../ModelProcessed"
-save_name     = "Compare_at_design" #sub_folder will be created
+save_name     = "Special_diagnostic_fuel" #sub_folder will be created
 # Fields to read out
-const fields = (:range_nmi,:PFEI_JJ,:lenFuseCyl_m,:FuelVolumeFractionACT,:lenACT_m)
+const fields = (:range_nmi,:PFEI_JJ,:lenFuseCyl_m,:FuelVolumeFractionACT,:lenACT_m,:volFuelTot_m3,:massEmpty_Ton,
+                :massFuelTot_Ton,:massPayload_Ton,:ene_fli_J,:volFuelMax_m3,:FuelVolumeFraction,:massTO_Ton)
 
 #### Create save directory
 save_dir_sub  = joinpath(save_dir,save_name)
@@ -47,3 +48,11 @@ plot_cases("Design Range","PFEI (J/J)",results,:range_nmi,:PFEI_JJ,caseNames,joi
 plot_cases("Design Range","Fuselage Cylinder Length (m)",results,:range_nmi,:lenFuseCyl_m,caseNames,joinpath(save_dir_sub, "LCylinder.png"))
 plot_cases("Design Range","Fractional Fuel in ACT",results,:range_nmi,:FuelVolumeFractionACT,caseNames,joinpath(save_dir_sub, "FractionFuelACT.png"))
 plot_cases("Design Range","Length of ACT (m)",results,:range_nmi,:lenACT_m,caseNames,joinpath(save_dir_sub, "ACTLength.png"))
+plot_cases("Design Range","Volume of Fuel (m³)",results,:range_nmi,:volFuelTot_m3,caseNames,joinpath(save_dir_sub, "FuelVolume.png"))
+plot_cases("Design Range","Mass of Fuel (Ton)",results,:range_nmi,:massFuelTot_Ton,caseNames,joinpath(save_dir_sub, "FuelMass.png"))
+plot_cases("Design Range","Empty Mass (Ton)",results,:range_nmi,:massEmpty_Ton,caseNames,joinpath(save_dir_sub, "EmptyMass.png"))
+plot_cases("Design Range","Payload Mass (Ton)",results,:range_nmi,:massPayload_Ton,caseNames,joinpath(save_dir_sub, "PayloadMass.png"))
+plot_cases("Design Range","Fuel Energy (J)",results,:range_nmi,:ene_fli_J,caseNames,joinpath(save_dir_sub, "FuelEnergy.png"))
+plot_cases("Design Range","Wing Inner Volume (m³)",results,:range_nmi,:volFuelMax_m3,caseNames,joinpath(save_dir_sub, "FuelVolumeWing.png"))
+plot_cases("Design Range","Fractional Fuel Volume Used",results,:range_nmi,:FuelVolumeFraction,caseNames,joinpath(save_dir_sub, "FracFuelVolumeUsed.png"))
+plot_cases("Design Range","Takeoff Mass (Ton)",results,:range_nmi,:massTO_Ton,caseNames,joinpath(save_dir_sub, "TakeeoffMass.png"))
