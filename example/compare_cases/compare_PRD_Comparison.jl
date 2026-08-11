794cfacf1839f6da21db1995bece9f8865f2069a
yang chen <yangchen@dhcp-10-31-135-26.dyn.MIT.EDU>
2026-08-11 16:58:07 -0400

    Update compare_cases for single point ethanol and jetfuel comparison and PRD comparison

--- example/compare_cases/compare_PRD_Comparison.jl
+++ example/compare_cases/compare_PRD_Comparison.jl
@@ -12,44 +12,131 @@ using .Extract: extract_acModel_compact!, init_results_2Layers, plot_cases_speci
 #### Setup IO
 # Input case names - Retrofit
 model_dir  = "../ModelSaved"
-caseKeys   = ["Opti_Jet_NoACT_V2_1_ULByEng_Aft3_PRD_","Opti_Jet_NoACT_V2_1_ULByEng_Eth_PRD_", "Opti_Eth_NoACT_V2_2_MatchR1R2R3_PRD_"]
-caseNames  = ["Baseline Jet Fuel"                    ,"Retrofitted Jet Fuel"                , "Optimized Ethanol"                   ]
-desran     = 3000
-offdesran  = collect(300:100:8000) 
+caseKeys   = ["Opti_Jet_NoACT_V4_4_R1Sz_EtasEng_PRD_", "Opti_Eth_NoACT_V4_3_R1Sz_EtasEng_PRD_"]
+caseNams   = ["Jet Fuel"                         , "Ethanol"                          ]
+desRans    = [1000,2000,3000]
+offDesRans = collect(300:100:8000) #Ensure that the first range is flyable across all the cases and all the design ranges
 # Output directory
-save_dir      = "../ModelProcessed"
-save_name     = "retrofitted_specially_sized_PRD" #sub_folder will be created
+save_dir   = "../ModelProcessed"
+save_name  = "Aug_3_2026_Jet_Fuel_vs_Ethanol_Conventional" #sub_folder will be created
 # Fields to read out
-const fields = [:(parm[imRange,2]),:(parm[imWpay,2])]
+const fields = [:(parm[imRange,2]),:(parm[imWpay,2]),:(parm[imPFEI,2])]
 
 #### Create save directory
 save_dir_sub  = joinpath(save_dir,save_name)
 mkpath(save_dir_sub)
 
 #### Initialization
-dataset = [init_results_2Layers(length(offdesran), fields) for _ in caseKeys] #[dataset[Expr][:],...]
+dataset = [[init_results_2Layers(length(offDesRans), fields) for _ in desRans] for _ in caseKeys] #dataset[idxCase][idxRange][ExprForField][IdxOffDesRange]
+datasetR1R2R3 = [[init_results_2Layers(3, fields) for _ in desRans] for _ in caseKeys] #dataset[idxCase][idxRange][ExprForField][index for R1 R2 R3]
 
 #### Extract data for the sized missions
-for (j, caseKey) in enumerate(caseKeys)
-    for (i, ran) in enumerate(offdesran)
-        # Read in the case
-        ac_dir = joinpath(model_dir,caseKey,caseKey*"$(round(Int,desran))_",caseKey*"$(round(Int,desran))_$(round(Int,ran)).jld2")
-        #
-        try
-            ac = quickload_aircraft(ac_dir)
-            extract_acModel_compact!(ac, dataset[j], i)
-            println("File, $(ac_dir), read successfully")
-            println("Data case $(j) at range $(ran) collected successfully")
-        catch
-            #
+for (idxCase, caseKey) in enumerate(caseKeys)
+    for (idxDesRan, desRange) in enumerate(desRans)
+        model_dir_sub = joinpath(model_dir,caseKey,caseKey*"$(desRange)_")
+        data = dataset[idxCase][idxDesRan]
+        dataR1R2R3 = datasetR1R2R3[idxCase][idxDesRan]
+        idxLast = 0
+        for (idxOffDesRange, offRange) in enumerate(offDesRans)
+            model_name = caseKey*"$(desRange)_"*"$(offRange).jld2"
+            model_path = glob(model_name, model_dir_sub)
+            if length(model_path)!=1
+                continue
+            end
+            model_path = only(model_path)
+            idxLast = idxOffDesRange
+            ac = quickload_aircraft(model_path)
+            extract_acModel_compact!(ac, data, idxOffDesRange)
         end
+        for v in values(data)
+            resize!(v,min(length(v),idxLast))
+        end
+        ####Extract R1 (Assume R1 R2 R3 all exist)
+        model_name = caseKey*"$(desRange)_"*"R1_*.jld2"
+        model_path = only(glob(model_name, model_dir_sub))
+        ac = quickload_aircraft(model_path)
+        extract_acModel_compact!(ac, dataR1R2R3, 1)
+        #R2
+        model_name = caseKey*"$(desRange)_"*"R2_*.jld2"
+        model_path = only(glob(model_name, model_dir_sub))
+        ac = quickload_aircraft(model_path)
+        extract_acModel_compact!(ac, dataR1R2R3, 2)
+        #R3
+        model_name = caseKey*"$(desRange)_"*"R3_*.jld2"
+        model_path = only(glob(model_name, model_dir_sub))
+        ac = quickload_aircraft(model_path)
+        extract_acModel_compact!(ac, dataR1R2R3, 3)
     end
 end
 
 #### Plotting
+# Used for plotting
+const LINESTYLES = [:solid, :dash, :dot, :dashdot, :dashdotdot]
+const LINECOLORS = [:blue, :red, :green, :orange, :purple, :brown, :black, :magenta, :cyan, :olive]
+const MARKERS    = [:rect, :circle, :diamond, :utriangle, :dtriangle]
 # PRD
-plot_cases_specified("Off-design Ranges [nmi]", "Payload Weight [Ton]", 
-                     [d[:(parm[imRange,2])] ./ 1852.0 for d in dataset],
-                     [d[:(parm[imWpay,2])] ./ 9.81 ./ 1000.0 for d in dataset],
-                     caseNames, 
-                     joinpath(save_dir_sub,"PRD_$(desran).png"))
+p = plot(xlabel="Off-design Ranges [nmi]", ylabel="Payload Weight [Ton]", dpi=800, legend=:best)
+for (idxCase,dataCase) in enumerate(dataset)
+    for (idxDesRan,dataDesign) in enumerate(dataCase)
+        # Setup style
+        i = (idxCase-1)*length(dataCase)+idxDesRan
+        color = LINECOLORS[mod1(i, length(LINECOLORS))]
+        marker = MARKERS[mod1(cld(i, length(LINECOLORS)), length(MARKERS))]
+        linestyle = LINESTYLES[mod1(cld(i, length(LINECOLORS) * length(MARKERS)), length(LINESTYLES))]
+        # Plot
+        plot!(p, dataDesign[:(parm[imRange,2])] ./ 1852.0,
+                 dataDesign[:(parm[imWpay,2])] ./ 9.81 ./ 1000.0, 
+                 marker=marker, mc=color, msc=color, color=color, lw=2, 
+                 linestyle=linestyle, label="$(caseNams[idxCase]) R1: $(desRans[idxDesRan]) nmi")
+    end
+end
+for (idxCase,dataCase) in enumerate(dataset)
+    for (idxDesRan,dataDesign) in enumerate(dataCase)
+        # Plot R1 R2 R3
+        scatter!(p, datasetR1R2R3[idxCase][idxDesRan][:(parm[imRange,2])] ./ 1852.0,
+                    datasetR1R2R3[idxCase][idxDesRan][:(parm[imWpay,2])] ./ 9.81 ./ 1000.0, 
+                    markershape=:circle, markersize=3, markercolor=:black, markerstrokecolor=:black, 
+                    markerstrokewidth=0, label=nothing)
+    end
+end
+xlims!(p, 0, 6000)
+ylims!(p, 0, 25)
+savefig(p,joinpath(save_dir_sub,"PRD.png"))
+
+# PRD
+p = plot(xlabel="Off-design Ranges [nmi]", ylabel="PFEI [J/J]", dpi=800, legend=:best)
+for (idxCase,dataCase) in enumerate(dataset)
+    for (idxDesRan,dataDesign) in enumerate(dataCase)
+        # Setup style
+        i = (idxCase-1)*length(dataCase)+idxDesRan
+        color = LINECOLORS[mod1(i, length(LINECOLORS))]
+        marker = MARKERS[mod1(cld(i, length(LINECOLORS)), length(MARKERS))]
+        linestyle = LINESTYLES[mod1(cld(i, length(LINECOLORS) * length(MARKERS)), length(LINESTYLES))]
+        # Plot
+        plot!(p, dataDesign[:(parm[imRange,2])] ./ 1852.0,
+                 dataDesign[:(parm[imPFEI,2])], 
+                 marker=marker, mc=color, msc=color, color=color, lw=2, 
+                 linestyle=linestyle, label="$(caseNams[idxCase]) R1: $(desRans[idxDesRan]) nmi")
+    end
+end
+xlims!(p, 0, 6000)
+ylims!(p, 0.6, 1)
+savefig(p,joinpath(save_dir_sub,"PFEI.png"))
+
+# for i in eachindex(xdata)
+#         length(xdata[i])==length(ydata[i]) || error("xy data for case $(i) dont have the same number of data points $(length(xdata[i])),$(length(ydata[i]))")
+#         color = LINECOLORS[mod1(i, length(LINECOLORS))]
+#         marker = MARKERS[mod1(cld(i, length(LINECOLORS)), length(MARKERS))]
+#         linestyle = LINESTYLES[mod1(cld(i, length(LINECOLORS) * length(MARKERS)), length(LINESTYLES))]
+#         plot!(p, xdata[i], ydata[i], marker=marker, mc=color, msc=color, color=color, lw=lw, linestyle=linestyle, label=datalab[i])
+#     end
+    
+
+
+# plot_cases_specified("Off-design Ranges [nmi]", "Payload Weight [Ton]",
+#                     [dataDesign[:(parm[imRange,2])] ./ 1852.0 for dataCase in dataset for dataDesign in dataCase],
+#                     [dataDesign[:(parm[imWpay,2])] ./ 9.81 ./ 1000.0 for dataCase in dataset for dataDesign in dataCase],
+#                     [nothing for caseCur in caseKeys for desRanCur in desRans],
+#                      joinpath(save_dir_sub,"PRD.png"))
+
+#                      #"$(caseCur) & R1: $(desRanCur) nmi"
--- example/compare_cases/compare_cases.jl
+++ example/compare_cases/compare_cases.jl
@@ -7,18 +7,31 @@ include(__TASOPTindices__)
 using Plots
 include(joinpath(@__DIR__,"../utilities_for_postprocessing/Extract.jl"))
 using .Extract: extract_acModel_compact!, init_results_2Layers, plot_cases_specified
+include(joinpath(@__DIR__,"../Breguet_range_solve_offdes.jl"))
+using .Breguet: Bre_off_des
 
 #### Setup IO
 # Input case names - Retrofit
 model_dir  = "../ModelSaved"
-caseKeys   = ["Opti_Jet_NoACT_",   "Opti_Jet_NoACT_V2_",  "Opti_Jet_NoACT_V3_", "Opti_Eth_NoACT_", "Opti_Eth_NoACT_V2_", "Opti_Eth_NoACT_V3_"]
-caseNames  = ["Jet Fuel V1",       "Jet Fuel V2",         "Jet Fuel V3",        "Ethanol V1",      "Ethanol V2"        , "Ethanol V3" ]
+caseKeys   = ["Opti_Jet_NoACT_V4_4_R1Sz_EtasEng_","Opti_Eth_NoACT_V4_3_R1Sz_EtasEng_"]
+caseNames  = ["Jet Fuel"                         ,"Ethanol"                          ]
 ranges     = collect(300:100:3000) 
 # Output directory
 save_dir      = "../ModelProcessed"
-save_name     = "baseline_compare" #sub_folder will be created
+save_name     = "Aug_3_2026_Jet_Fuel_vs_Ethanol_Conventional" #sub_folder will be created
 # Fields to read out
-const fields = [:(parm[imRange,1]),:(parm[imPFEI,1])]
+const fields = [:(parm[imRange,1]),:(parm[imPFEI,1]),:(parm[imVfuel,1]),:(parg[igVfmax]),
+                :(wing.layout.span),:(wing.outboard.λ),:(wing.layout.ηs),:(wing.layout.AR),
+                :(para[iaCL,ipcruise1,1]),:(para[iaCL,ipcruise2,1]),
+                :(para[iaCD,ipcruise1,1]),:(para[iaCD,ipcruise2,1]),
+                :(pare[iehfuel,ipcruise1,1]),:(pare[iehfuel,ipcruise2,1]),
+                :(pare[ieTSFC,ipcruise1,1]),:(pare[ieTSFC,ipcruise2,1]),
+                :(para[iagamV,ipcruise1,1]),:(para[iagamV,ipcruise2,1]),
+                :(pare[ieu0,ipcruise1,1]),:(pare[ieu0,ipcruise2,1]),
+                :(parm[imWTO,1]),:(parm[imWfuel,1]),:(parm[imWpay,1]),:(parg[igrhofuel]),:(parg[igfreserve]),
+                :(wing.layout.S)]
+
+
 
 #### Create save directory
 save_dir_sub  = joinpath(save_dir,save_name)
@@ -41,8 +54,113 @@ for (j, caseKey) in enumerate(caseKeys)
 end
 
 #### Plotting
-plot_cases_specified("Design Range [nmi]", "PFEI [J/J]", 
+plot_cases_specified("Design/R1 Range [nmi]", "Flight PFEI [J/J]", 
                      [d[:(parm[imRange,1])] for d in dataset] ./ 1852.0, 
                      [d[:(parm[imPFEI,1])] for d in dataset],
                      caseNames, 
                      joinpath(save_dir_sub,"PFEI.png"))
+#
+plot_cases_specified("Design/R1 Range [nmi]", "Fuel Level [%]", 
+                     [d[:(parm[imRange,1])] for d in dataset] ./ 1852.0, 
+                     [d[:(parm[imVfuel,1])] .* 100.0 ./ d[:(parg[igVfmax])] for d in dataset],
+                     caseNames, 
+                     joinpath(save_dir_sub,"Fuel_Level.png"))
+#
+plot_cases_specified("Design/R1 Range [nmi]", "Wingspan [m]", 
+                     [d[:(parm[imRange,1])] for d in dataset] ./ 1852.0, 
+                     [d[:(wing.layout.span)] for d in dataset],
+                     caseNames, 
+                     joinpath(save_dir_sub,"Wingspan.png"))
+#
+plot_cases_specified("Design/R1 Range [nmi]", "Taper Ratio of Outer Wing Panel", 
+                     [d[:(parm[imRange,1])] for d in dataset] ./ 1852.0, 
+                     [d[:(wing.outboard.λ)] for d in dataset],
+                     caseNames, 
+                     joinpath(save_dir_sub,"OuterTaperWing.png"))
+#
+plot_cases_specified("Design/R1 Range [nmi]", "Wing Span Break Location [%]", 
+                     [d[:(parm[imRange,1])] for d in dataset] ./ 1852.0, 
+                     [d[:(wing.layout.ηs)] .* 100.0 for d in dataset],
+                     caseNames, 
+                     joinpath(save_dir_sub,"SpanBreakWing.png"))
+#
+plot_cases_specified("Design/R1 Range [nmi]", "Wing Aspect Ratio", 
+                     [d[:(parm[imRange,1])] for d in dataset] ./ 1852.0, 
+                     [d[:(wing.layout.AR)]  for d in dataset],
+                     caseNames, 
+                     joinpath(save_dir_sub,"AspectRatioWing.png"))
+#
+plot_cases_specified("Design/R1 Range [nmi]", "Lift-to-drag Ratio at Start of Cruise", 
+                     [d[:(parm[imRange,1])] for d in dataset] ./ 1852.0, 
+                     [d[:(para[iaCL,ipcruise1,1])] ./ d[:(para[iaCD,ipcruise1,1])]  for d in dataset],
+                     caseNames, 
+                     joinpath(save_dir_sub,"LD_StartOfCruise.png"))
+#
+plot_cases_specified("Design/R1 Range [nmi]", "Lift-to-drag Ratio at End of Cruise", 
+                     [d[:(parm[imRange,1])] for d in dataset] ./ 1852.0, 
+                     [d[:(para[iaCL,ipcruise2,1])] ./ d[:(para[iaCD,ipcruise2,1])]  for d in dataset],
+                     caseNames, 
+                     joinpath(save_dir_sub,"LD_EndOfCruise.png"))
+#
+plot_cases_specified("Design/R1 Range [nmi]", "Engine Total Efficiency at Start of Cruise [%]", 
+                     [d[:(parm[imRange,1])] for d in dataset] ./ 1852.0, 
+                     [100.0 .* (1.0 ./ (d[:(pare[ieTSFC,ipcruise1,1])] ./ gee)) .* ((cos.(d[:(para[iagamV,ipcruise1,1])]) .* d[:(pare[ieu0,ipcruise1,1])]) ./ d[:(pare[iehfuel,ipcruise1,1])])  for d in dataset],
+                     caseNames, 
+                     joinpath(save_dir_sub,"TotEffEngine_StartOfCruise.png"))
+#
+plot_cases_specified("Design/R1 Range [nmi]", "Engine Total Efficiency at End of Cruise [%]", 
+                     [d[:(parm[imRange,1])] for d in dataset] ./ 1852.0, 
+                     [100.0 * (1.0 ./ (d[:(pare[ieTSFC,ipcruise2,1])] ./ gee)) .* ((cos.(d[:(para[iagamV,ipcruise2,1])]) .* d[:(pare[ieu0,ipcruise2,1])]) ./ d[:(pare[iehfuel,ipcruise2,1])])  for d in dataset],
+                     caseNames, 
+                     joinpath(save_dir_sub,"TotEffEngine_EndOfCruise.png"))
+#
+plot_cases_specified("Design/R1 Range [nmi]", "Aircraft Empty Weight [Ton]", 
+                     [d[:(parm[imRange,1])] for d in dataset] ./ 1852.0, 
+                     [(d[:(parm[imWTO,1])] .- d[:(parm[imWfuel,1])] .- d[:(parm[imWpay,1])]) ./ gee ./ 1000.0  for d in dataset],
+                     caseNames, 
+                     joinpath(save_dir_sub,"EmptyWeight.png"))
+#
+plot_cases_specified("Design/R1 Range [nmi]", "Fuel Weight Fraction of Total Takeoff Weight [%]", 
+                     [d[:(parm[imRange,1])] for d in dataset] ./ 1852.0, 
+                     [100.0 .* (d[:(parm[imWfuel,1])] ./ d[:(parm[imWTO,1])]) for d in dataset],
+                     caseNames, 
+                     joinpath(save_dir_sub,"FuelWeightFraction.png"))
+#
+plot_cases_specified("Design/R1 Range [nmi]", "Wing Surface Area [m²]", 
+                     [d[:(parm[imRange,1])] for d in dataset] ./ 1852.0, 
+                     [d[:(wing.layout.S)] for d in dataset],
+                     caseNames, 
+                     joinpath(save_dir_sub,"WingSurfaceArea.png"))
+#
+
+#### Special comparative plots
+#Calculate using Breguet range an estimated penalty for ethanol versus jet fuel use
+PFEIEthPenalty_Breguet = zeros(length(ranges)) #[%]
+iRC = 1 #Index Reference Case
+iRM = 28 #Index Reference Mission
+iRJ = 1 #Index of the jet case
+iRE = 2 #Index of the ethanol case
+for idxMiss in eachindex(ranges) #Go through each mission pair between ethanol and jet fuel case
+    outJet = Bre_off_des(dataset[iRC][:(parm[imRange,1])][idxMiss], dataset[iRC][:(parm[imWpay,1])][idxMiss];
+                         LD = dataset[iRC][:(para[iaCL,ipcruise1,1])][iRM]/dataset[iRC][:(para[iaCD,ipcruise1,1])][iRM],
+                         eta = (1.0/(dataset[iRC][:(pare[ieTSFC,ipcruise1,1])][iRM]/gee))*((cos(dataset[iRC][:(para[iagamV,ipcruise1,1])][iRM])*dataset[iRC][:(pare[ieu0,ipcruise1,1])][iRM])/dataset[iRC][:(pare[iehfuel,ipcruise1,1])][iRM]),
+                         LHV_Jkg = dataset[iRJ][:(pare[iehfuel,ipcruise1,1])][iRM], 
+                         wEmp_N = dataset[iRC][:(parm[imWTO,1])][iRM]-dataset[iRC][:(parm[imWfuel,1])][iRM]-dataset[iRC][:(parm[imWpay,1])][iRM],
+                         rhoFuel_kgm3 = dataset[iRJ][:(parg[igrhofuel])][iRM], frac_rese=dataset[iRC][:(parg[igfreserve])][iRM],
+                         wTO_Max_N=4e10, wPay_Max_N=1e10, volFuel_Max_m3=1e10/1e5) #Not considering physical limitations
+    outEth = Bre_off_des(dataset[iRC][:(parm[imRange,1])][idxMiss], dataset[iRC][:(parm[imWpay,1])][idxMiss];
+                         LD = dataset[iRC][:(para[iaCL,ipcruise1,1])][iRM]/dataset[iRC][:(para[iaCD,ipcruise1,1])][iRM],
+                         eta = (1.0/(dataset[iRC][:(pare[ieTSFC,ipcruise1,1])][iRM]/gee))*((cos(dataset[iRC][:(para[iagamV,ipcruise1,1])][iRM])*dataset[iRC][:(pare[ieu0,ipcruise1,1])][iRM])/dataset[iRC][:(pare[iehfuel,ipcruise1,1])][iRM]),
+                         LHV_Jkg = dataset[iRE][:(pare[iehfuel,ipcruise1,1])][iRM], 
+                         wEmp_N = dataset[iRC][:(parm[imWTO,1])][iRM]-dataset[iRC][:(parm[imWfuel,1])][iRM]-dataset[iRC][:(parm[imWpay,1])][iRM],
+                         rhoFuel_kgm3 = dataset[iRE][:(parg[igrhofuel])][iRM], frac_rese=dataset[iRC][:(parg[igfreserve])][iRM],
+                         wTO_Max_N=4e10, wPay_Max_N=1e10, volFuel_Max_m3=1e10/1e5) #Not considering physical limitations
+    PFEIEthPenalty_Breguet[idxMiss] = 100.0*(outEth["PFEI_JJ_out"]-outJet["PFEI_JJ_out"])/outJet["PFEI_JJ_out"]
+end
+
+plot_cases_specified("Design/R1 Range [nmi]", "Ethanol Flight PFEI Penalty [%]", 
+                     [dataset[iRC][:(parm[imRange,1])],dataset[iRC][:(parm[imRange,1])]] ./ 1852.0, 
+                     [100.0 .* (dataset[iRE][:(parm[imPFEI,1])] .- dataset[iRJ][:(parm[imPFEI,1])]) ./ dataset[iRJ][:(parm[imPFEI,1])],PFEIEthPenalty_Breguet],
+                     ["Optimized Parameters","Constant Efficiencies (Breguet)"], 
+                     joinpath(save_dir_sub,"EthPFEIPenalty.png"))
+#
